9bb4774700c853d45bc2fe879318b7e83d96b502
Eva Boogaard <evaboogaard@hotmail.com>
2023-09-21 16:02:10 +0200

    (style): epic animations

---
+++ scripts/goldband.js
@@ -0,0 +1,53 @@
+// Inspo: https://alvarotrigo.com/blog/css-text-animations/
+
+var words = [
+  'De zuster die komt dichterbij',
+  'Dit moet een delirium zijn',
+  'Want die zuster, dat ben jij',
+  'Dit is een noodgeval',
+  'Waar blijft die fucking ambulance?',
+];
+var part;
+var i = 0;
+var offset = 0;
+var len = words.length;
+var forwards = true;
+var skip_count = 0;
+var skip_delay = 15;
+var speed = 70;
+
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    document.querySelector('.word').textContent = part;
+  }, speed);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  wordflick();
+});
--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,4 @@ console.log('delirium!');
 
 import '../scripts/cursor.js';
 import '../scripts/content.js';
+import './goldband.js';
